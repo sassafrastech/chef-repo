694d2661fd6f9f6f07395ead65bbb3af3cf337fb
Tom Smyth <tom@sassafras.coop>
2017-03-10 11:29:45 -0500

    Fixed mysql backup recipe

--- vendor/cookbooks/mysql_extras/recipes/backup.rb
+++ vendor/cookbooks/mysql_extras/recipes/backup.rb
@@ -6,8 +6,8 @@ end
 
 cron "mysql_backup" do
   minute "0"
-  user "deploy"
-  rootpwd = node[:mysql][:server_root_password]
+  user "root"
+  rootpwd = node[:mysql][:root_password]
   dump_command = "mysqldump -u root -p#{rootpwd} elmo"
   command %Q{/bin/bash -l -c '/opt/db-backup/db-backup.rb ./db-backups "#{dump_command}" >> backup.log 2>&1'}
 end
