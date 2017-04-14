48520873133d6b20d99ddd526388960d0c40453e
Tom Smyth <tom@sassafras.coop>
2017-04-14 14:46:25 -0400

    Added elmo theme stuff

---
+++ vendor/cookbooks/elmo/Readme.md
@@ -0,0 +1,3 @@
+# ELMO Chef cookbook
+
+* To use a theme, ensure it exists in `vendor/cookbooks/elmo/files/themes/<theme_name>`, then set `"theme": "<theme_name>"` in the node under `elmo_config`.
--- vendor/cookbooks/elmo/recipes/config.rb
+++ vendor/cookbooks/elmo/recipes/config.rb
@@ -2,7 +2,10 @@ applications_root = node[:rails][:applications_root]
 
 if node[:active_applications]
   node[:active_applications].each do |app, app_info|
-    config_dir = "#{applications_root}/#{app}/shared/config"
+    shared_dir = "#{applications_root}/#{app}/shared"
+    config_dir = "#{shared_dir}/config"
+    elmo_config = app_info["elmo_config"]
+
     directory config_dir do
       owner "deploy"
       group "deploy"
@@ -14,7 +17,7 @@ if node[:active_applications]
       owner "deploy"
       group "deploy"
       mode 0600
-      variables app_info["elmo_config"]
+      variables elmo_config
     end
 
     template "#{config_dir}/thinking_sphinx.yml" do
@@ -22,6 +25,35 @@ if node[:active_applications]
       owner "deploy"
       group "deploy"
       mode 0600
+    end
+
+    if theme = elmo_config["theme"]
+      variables_dir = "#{shared_dir}/app/assets/stylesheets/all/variables"
+      images_dir = "#{shared_dir}/app/assets/images"
+
+      [variables_dir, images_dir].each do |dir|
+        directory dir do
+          owner "deploy"
+          group "deploy"
+          recursive true
+        end
+      end
+
+      cookbook_file "#{variables_dir}/_theme.scss" do
+        source "themes/#{theme}/_theme.scss"
+        owner "deploy"
+        group "deploy"
+        mode 0600
+        action :create
+      end
+
+      cookbook_file "#{images_dir}/logo-override.png" do
+        source "themes/#{theme}/logo.png"
+        owner "deploy"
+        group "deploy"
+        mode 0600
+        action :create
+      end
     end
   end
 end
